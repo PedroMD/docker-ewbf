d49ef506e45228ccd9d040bc32853400ab20af38
PedroMD <PedroMD@users.noreply.github.com>
2017-11-12 23:00:44 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-echo "Waiting ${STARTUP_DELAY} seconds"
+#echo "Waiting ${STARTUP_DELAY} seconds"
 
-sleep ${STARTUP_DELAY}
+#sleep ${STARTUP_DELAY}
 
 echo "Starting miner"
 
-./miner --server ${SERVER} --user ${ADDRESS}.${WORKER} --pass ${PASS} --port ${PORT} --api 0.0.0.0:42000
+./miner --server $1 --user $2.$3 --pass $4 --port $5
